7483409e548d42cbaf16b336160d0170fd9d3555
josarapar <josearanda9000@gmail.com>
2025-09-17 19:39:42 +0200

    Metodos agregados sumaTotalPaginas, listarLibrosMasPaginasPromedio funcionando correctamente

--- src/main/java/org/semana01/modelos/ColeccionLibros.java
+++ src/main/java/org/semana01/modelos/ColeccionLibros.java
@@ -51,6 +51,26 @@ public class ColeccionLibros {
         }
         return titulos;
     }
+
+    public int sumaTotalPaginas() {
+        int totalPaginas = 0;
+        for (int i=0;i<libros.toArray().length;i++){
+            totalPaginas = totalPaginas + libros.get(i).getPaginas();
+        }
+        return totalPaginas;
+    }
+
+    public String listarLibrosMasPaginasPromedio() {
+        String titulos = "";
+        int promedio = sumaTotalPaginas() / libros.toArray().length;
+        libros.sort(Comparator.comparingInt(Libro::getPaginas).reversed());
+        for (int i = 0; i< libros.toArray().length; i++) {
+            if (promedio>= libros.get(i).getPaginas()){
+                titulos = titulos + ", " + libros.get(i).getTitulo();
+            }
+        }
+        return titulos;
+    }
     // ¿Qué tipo de colección es la más adecuada para almacenar los libros?
 
     // Crea los métodos solicitados en el enunciado del ejercicio
